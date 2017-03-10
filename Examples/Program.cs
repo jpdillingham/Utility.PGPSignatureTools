f2e9f517d81569a9d2f9e8107b099096408cfd6f
JP Dillingham <jpdillingham@gmail.com>
2017-03-10 02:03:14 -0600

    Implemented logic and examples for Sign/Verify

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Examples
+namespace Utility.PGPSignatureTools.Examples
 {
-    class Program
+    /// <summary>
+    ///     Demonstrates the signing and verification of a payload using the PGP private and public keys included in the example files.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        #region Private Fields
+
+        /// <summary>
+        ///     The password for the private key file.
+        /// </summary>
+        private const string Password = "8&$gy$8rrPO^tbE1m5";
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        /// <summary>
+        ///     Runs the example.
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
         {
+            string text = "hello world!";
+
+            Console.WriteLine("Signing payload: " + text);
+
+            // create the PGP signature
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+            byte[] signature = PGPSignature.Sign(bytes, File.ReadAllText(@"Keys\privateKey.asc"), Password);
+
+            Console.WriteLine("\nPGP Signature:\n");
+            Console.WriteLine(Encoding.ASCII.GetString(signature));
+
+            Console.WriteLine("\nValidating signature and retrieving payload:\n");
+
+            // verify the signature and store the message payload in the message byte array
+            byte[] message = PGPSignature.Verify(signature, File.ReadAllText(@"Keys\publicKey.asc"));
+
+            Console.WriteLine("Retrieved payload: " + Encoding.ASCII.GetString(message));
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
         }
+
+        #endregion Private Methods
     }
 }
