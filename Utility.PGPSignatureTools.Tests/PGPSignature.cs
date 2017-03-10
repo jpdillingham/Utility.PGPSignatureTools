874a0e3f3725d209c9961b7e32f5b7e99e670055
JP Dillingham <jpdillingham@gmail.com>
2017-03-10 02:22:21 -0600

    Added xunit VS runner.  Created first unit test.

---
+++ Utility.PGPSignatureTools.Tests/PGPSignature.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Utility.PGPSignatureTools.Tests
+{
+    /// <summary>
+    ///     Unit tests for the <see cref="PGPSignature"/> class.
+    /// </summary>
+    public class PGPSignature
+    {
+        /// <summary>
+        ///     The password for the private key file.
+        /// </summary>
+        private const string Password = "8&$gy$8rrPO^tbE1m5";
+
+        [Fact]
+        public void Sign()
+        {
+            string text = "hello world!";
+
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+            byte[] signature = PGPSignatureTools.PGPSignature.Sign(bytes, File.ReadAllText(@"Keys\privateKey.asc"), Password);
+
+            Assert.NotNull(Encoding.ASCII.GetString(signature));
+        }
+    }
+}
