7e27e087408eec00598f6da7563d403687445c31
JP Dillingham <jpdillingham@gmail.com>
2017-03-26 18:00:45 -0500

    Updated assemblyinfo

--- Utility.PGPSignatureTools/Properties/AssemblyInfo.cs
+++ Utility.PGPSignatureTools/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes. Change these attribute values to
-// modify the information associated with an assembly.
 [assembly: AssemblyTitle("Utility.PGPSignatureTools")]
 [assembly: AssemblyDescription("A C# Class Library for signing and verifying data using PGP encryption.")]
 [assembly: AssemblyConfiguration("")]
@@ -12,18 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) 2017 JP Dillingham (jp@dillingham.ws)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible to COM components. If you need to access a type in this
-// assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3d60ee4b-3d87-4ae1-8761-72f6df5ca577")]
-
-// Version information for an assembly consists of the following four values:
-//
-// Major Version Minor Version Build Number Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
